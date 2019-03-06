c61bfd8c185a162bd1856fc643f17148dcd0df99
2tallyork <2tallyork@users.noreply.github.com>
2019-03-05 20:45:24 -0500

    first commit

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,5 +1,5 @@
 <?php namespace App\Http\Controllers;
-
+//this is the movie controller
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateMovieValidation;
 
@@ -19,10 +19,10 @@ class MovieController extends Controller
     public function index()
     {
         $movies = Movie::get()->toArray();
-        
+
         return view('movies.index')->with('movies', $movies);
     }
-    
+
     /**
      * Show the form for creating a new resource.
      *
@@ -32,7 +32,7 @@ class MovieController extends Controller
     {
         return view('movies.create');
     }
-    
+
     /**
      * Store a newly created resource in storage.
      *
@@ -42,21 +42,21 @@ class MovieController extends Controller
     public function store(CreateMovieValidation $request)
     {
         $input = $request->all();
-      
+
         $movie = new Movie($input);
         if ( ! $movie->save() ) {
             $this->setMessage('Oops... Movie was not created.  Please try again', 'error');
             return redirect('/movies');
         }
-        
+
         // Save the uploaded photo
         if ($request->file('cover_photo')) {
             $this->savePicture($request, $movie);
         }
-        
+
         return redirect('/movies');
     }
-    
+
     /**
      * Uses the image library from Intervention to manipulate the image See
      * example 14-4 in book.
@@ -68,20 +68,20 @@ class MovieController extends Controller
     private function savePicture($request, Movie $movie)
     {
         $original = $request->file('cover_photo');
-        
+
         $image = Image::make($original)->encode('jpg', 75);
-        
+
         $filename = 'movie_' . $movie->id . '.jpg';
-        
+
         // For updates we need to delete existing file
         if (Storage::disk('web')->exists($filename)) {
             Storage::disk('web')->delete($filename);
         }
-        
+
         Storage::disk('web')->put($filename, $image->getEncoded());
     }
-    
-    
+
+
     /**
      * Display the specified resource.
      *
@@ -92,29 +92,29 @@ class MovieController extends Controller
     {
         // if ( $movie = Movie::with('genre')->with('reviews')->find($id)->toArray() ) {
         if ( $movie = Movie::find($id)->toArray() ) {
-            
+
             // $genres = Genre::get()->toArray();
-            
+
             // $avgKernals = $this->calculateKernals($movie['reviews']);
-            
+
             return view('movies.show')->with('movie', $movie);
                 // ->with('genres', $genres)
                 // ->with('kernals', $avgKernals);
         }
-        
+
         // User doesn't exist for this account
         $this->setMessage('Movie not found!', 'error');
-        
+
         return redirect('/movies');
     }
-    
-    
+
+
     // Get a list of movies to view as a renter
     public function listing()
     {
         $movies = Movie::orderBy('title')->get()->toArray();
-        
+
         return view('movies.listing')->with('movies', $movies);
     }
-    
+
 }
