46e5e7acc6bb119cd282c00282d90b3f49a4d6eb
2tallyork <2tallyork@users.noreply.github.com>
2019-04-16 19:30:16 -0400

    most recentest commit

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', 'MovieController@listing')->name('listing');
 Route::group(['middleware' => 'auth'], function () {
     Route::post('/movies/rent', 'RentalController@rent');
     Route::resource('/movies', 'MovieController');
-    
+
     Route::get('/rentals/history', 'RentalController@rentalHistory')->name('history');
     Route::get('/rentals/return', 'RentalController@returnMovie')->name('return');
     Route::post('/rentals/processReturn', 'RentalController@processReturn')->name('processReturn');
